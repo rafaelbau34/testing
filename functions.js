e9ef20886eff8257010a130babb1ea2dd8f7a0d1
rafaelbau34 <rafaelbaugar@gmail.com>
2025-11-25 21:57:10 -0600

    caesar cipher function

--- functions.js
+++ functions.js
@@ -34,3 +34,31 @@ const calculator = {
     return a / b;
   },
 };
+
+function caesarCipher(str, shiftFactor) {
+  const shift = shiftFactor % 26;
+
+  const result = [...str]
+    .map((char) => {
+      const charCode = char.charCodeAt(0);
+      const shiftChar = (base) => {
+        return ((charCode - base + shift + 26) % 26) + base;
+      };
+
+      if (charCode >= 65 && charCode <= 90) {
+        return String.fromCharCode(shiftChar(65));
+      } else if (charCode >= 97 && charCode <= 122) {
+        return String.fromCharCode(shiftChar(97));
+      } else {
+        return char;
+      }
+    })
+    .join("");
+  return result;
+}
+
+console.log(`Caesar Cipher ('abc', 1): ${caesarCipher("abc", 1)}`); // Output: 'bcd'
+console.log(`Caesar Cipher ('xyz', 3): ${caesarCipher("xyz", 3)}`); // Output: 'abc'
+console.log(
+  `Caesar Cipher ('Hello, World!', 5): ${caesarCipher("Hello, World!", 5)}`
+); // Output: 'Mjqqt, Btwqi!'
